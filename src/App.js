06caa3eaf56d3bf7b138008ccded01e289c842d0
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2021-06-20 15:16:36 +0500

    added react-query dev tools

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import Navbar from "./components/Navbar";
 import Planets from "./components/Planets";
 import People from "./components/People";
 import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   const [page, setPage] = useState("planets");
   return (
-    <div className="App">
-      <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className="content">
-        {page === "planets" ? <Planets /> : <People />}
-      </div>
-    </div>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <h1>Star Wars Info</h1>
+          <Navbar setPage={setPage} />
+          <div className="content">
+            {page === "planets" ? <Planets /> : <People />}
+          </div>
+        </div>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
   );
 }
 
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,7 +1,29 @@
+import { useQuery } from "react-query";
+import Planet from "./Planet";
+
+const fetchPlanets = async () => {
+  const res = await fetch("http://swapi.dev/api/planets/");
+  return res.json();
+};
+
 const Planets = () => {
+  const { data, status } = useQuery("planets", fetchPlanets, {
+    onSuccess: () => console.log("data succesfully fetched"),
+    onError: () => console.log("Error fetching data"),
+  });
+  console.log(status);
   return (
     <div>
       <h2>Planets</h2>
+      {status === "loading" && <div>Loading data</div>}
+      {status === "error" && <div>Error fetching data</div>}
+      {status === "success" && (
+        <div>
+          {data.results.map((planet) => (
+            <Planet key={planet.name} planet={planet} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
